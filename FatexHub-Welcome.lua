eebacb3d275705a55be26ec236d930bd4ff0a82d
New-Exploiter <85951383+New-Exploiter@users.noreply.github.com>
2021-12-19 23:51:31 -0500

    Update FatexHub-Welcome.lua

--- FatexHub-Welcome.lua
+++ FatexHub-Welcome.lua
@@ -1,1 +1,302 @@
+local Fatex_Hub = Instance.new("ScreenGui")
+local main = Instance.new("ImageButton")
+local Frame = Instance.new("ImageButton")
+local Text = Instance.new("TextLabel")
+local Text_2 = Instance.new("TextLabel")
+local Ignore_2 = Instance.new("TextLabel")
+local Frame_2 = Instance.new("Frame")
+local Frame_3 = Instance.new("Frame")
+local Frame_4 = Instance.new("Frame")
+local Frame_5 = Instance.new("Frame")
+local Ignore = Instance.new("TextLabel")
+local Frame_6 = Instance.new("Frame")
+local Frame_7 = Instance.new("Frame")
+local Frame_8 = Instance.new("Frame")
+local Frame_9 = Instance.new("Frame")
+local Frame_10 = Instance.new("ImageButton")
+local Text_3 = Instance.new("TextLabel")
+local Button = Instance.new("ImageButton")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local Frame_11 = Instance.new("ImageButton")
+local Text_4 = Instance.new("TextLabel")
+local Text_5 = Instance.new("TextLabel")
 
+
+Fatex_Hub.Name = "Fatex_Hub"
+
+syn.protect_gui(main)
+
+Fatex_Hub.Parent = game.CoreGui
+
+main.Name = "main"
+main.Parent = Fatex_Hub
+main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+main.BackgroundTransparency = 1.000
+main.Position = UDim2.new(0.266720384, 0, 0.355129629, 0)
+main.Size = UDim2.new(0, 700, 0, 352)
+main.Image = "rbxassetid://2790389767"
+main.ScaleType = Enum.ScaleType.Slice
+main.SliceCenter = Rect.new(8, 8, 248, 248)
+main.SliceScale = 0.500
+
+Frame.Name = "Frame"
+Frame.Parent = main
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.Position = UDim2.new(0.0171428565, 0, 0.174204573, 0)
+Frame.Size = UDim2.new(0, 675, 0, 72)
+Frame.Image = "rbxassetid://2790382281"
+Frame.ImageColor3 = Color3.fromRGB(240, 240, 240)
+Frame.ScaleType = Enum.ScaleType.Slice
+Frame.SliceCenter = Rect.new(4, 4, 252, 252)
+
+Text.Name = "Text"
+Text.Parent = main
+Text.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Text.BackgroundTransparency = 1.000
+Text.Position = UDim2.new(0.0199999996, 0, 0.174204573, 0)
+Text.Size = UDim2.new(0, 675, 0, 72)
+Text.Font = Enum.Font.GothamSemibold
+Text.Text = " Hello dear user. This is the Fatex Hub (Paid), this hub           work on Synapse X,  if there's any bugs contact Fate#4752. Don't forget that you can get banned so use an alt, ALT BOT V2 is recommended if you want to go in games that have high moderation and if your very blatant (easy to detect if hacking)."
+Text.TextColor3 = Color3.fromRGB(0, 0, 0)
+Text.TextSize = 18.000
+Text.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Text.TextWrapped = true
+Text.TextXAlignment = Enum.TextXAlignment.Left
+Text.TextYAlignment = Enum.TextYAlignment.Top
+
+Text_2.Name = "Text"
+Text_2.Parent = main
+Text_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Text_2.BackgroundTransparency = 1.000
+Text_2.Position = UDim2.new(0.659, 0, 0.157, 0)
+Text_2.Size = UDim2.new(0, 54, 0, 31)
+Text_2.Font = Enum.Font.GothamBold
+Text_2.Text = "ONLY"
+Text_2.TextColor3 = Color3.fromRGB(255, 0, 0)
+Text_2.TextSize = 18.000
+Text_2.TextStrokeColor3 = Color3.fromRGB(255, 0, 0)
+Text_2.TextWrapped = true
+
+Ignore_2.Name = "Ignore_2"
+Ignore_2.Parent = main
+Ignore_2.AnchorPoint = Vector2.new(0.5, 0.5)
+Ignore_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Ignore_2.BackgroundTransparency = 1.000
+Ignore_2.BorderSizePixel = 0
+Ignore_2.Position = UDim2.new(0.422386914, 0, 0.0874153748, 0)
+Ignore_2.Size = UDim2.new(0.170940459, -5, 0.103401318, -5)
+Ignore_2.Font = Enum.Font.GothamSemibold
+Ignore_2.Text = "Fatex"
+Ignore_2.TextColor3 = Color3.fromRGB(0, 0, 167)
+Ignore_2.TextSize = 45.000
+
+Frame_2.Parent = main
+Frame_2.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+Frame_2.BorderColor3 = Color3.fromRGB(0, 0, 255)
+Frame_2.Position = UDim2.new(0.504285753, 0, 0.0285227261, 0)
+Frame_2.Size = UDim2.new(0, 0, 0, 43)
+
+Frame_3.Parent = main
+Frame_3.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Frame_3.BorderColor3 = Color3.fromRGB(0, 0, 255)
+Frame_3.Position = UDim2.new(0.339999974, 0, 0.151379779, 0)
+Frame_3.Size = UDim2.new(0, 115, 0, 0)
+
+Frame_4.Parent = main
+Frame_4.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+Frame_4.BorderColor3 = Color3.fromRGB(0, 0, 255)
+Frame_4.Position = UDim2.new(0.339999944, 0, 0.0285227261, 0)
+Frame_4.Size = UDim2.new(0, 0, 0, 41)
+
+Frame_5.Parent = main
+Frame_5.BackgroundColor3 = Color3.fromRGB(0, 0, 255)
+Frame_5.BorderColor3 = Color3.fromRGB(0, 0, 255)
+Frame_5.Position = UDim2.new(0.340000004, 0, 0.0285227261, 0)
+Frame_5.Size = UDim2.new(0, 115, 0, 0)
+
+Ignore.Name = "Ignore"
+Ignore.Parent = main
+Ignore.AnchorPoint = Vector2.new(0.5, 0.5)
+Ignore.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Ignore.BackgroundTransparency = 1.000
+Ignore.BorderSizePixel = 0
+Ignore.Position = UDim2.new(0.585434556, 0, 0.0848303661, 0)
+Ignore.Size = UDim2.new(0.129440486, -5, 0.108571425, -5)
+Ignore.Font = Enum.Font.GothamSemibold
+Ignore.Text = "Hub"
+Ignore.TextColor3 = Color3.fromRGB(191, 0, 0)
+Ignore.TextSize = 45.000
+
+Frame_6.Parent = main
+Frame_6.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame_6.BorderColor3 = Color3.fromRGB(255, 0, 0)
+Frame_6.Position = UDim2.new(0.513571441, 0, 0.151379883, 0)
+Frame_6.Size = UDim2.new(0, 102, 0, 0)
+
+Frame_7.Parent = main
+Frame_7.AnchorPoint = Vector2.new(1, 1)
+Frame_7.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Frame_7.BorderColor3 = Color3.fromRGB(255, 0, 0)
+Frame_7.Position = UDim2.new(0.513571441, 0, 0.151379794, 0)
+Frame_7.Size = UDim2.new(0, 0, 0, 43)
+
+Frame_8.Parent = main
+Frame_8.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Frame_8.BorderColor3 = Color3.fromRGB(255, 0, 0)
+Frame_8.Position = UDim2.new(0.513571441, 0, 0.0285227261, 0)
+Frame_8.Size = UDim2.new(0, 102, 0, 0)
+
+Frame_9.Parent = main
+Frame_9.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+Frame_9.BorderColor3 = Color3.fromRGB(255, 0, 0)
+Frame_9.Position = UDim2.new(0.659999967, 0, 0.0342370123, 0)
+Frame_9.Size = UDim2.new(0, 0, 0, 41)
+
+Frame_10.Name = "Frame"
+Frame_10.Parent = main
+Frame_10.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_10.BackgroundTransparency = 1.000
+Frame_10.Position = UDim2.new(0.0171428584, 0, 0.410000026, 0)
+Frame_10.Size = UDim2.new(0, 675, 0, 38)
+Frame_10.Image = "rbxassetid://2790382281"
+Frame_10.ImageColor3 = Color3.fromRGB(240, 240, 240)
+Frame_10.ScaleType = Enum.ScaleType.Slice
+Frame_10.SliceCenter = Rect.new(4, 4, 252, 252)
+
+Text_3.Name = "Text"
+Text_3.Parent = Frame_10
+Text_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Text_3.BackgroundTransparency = 1.000
+Text_3.Position = UDim2.new(-0.000634919852, 0, -0.0138934851, 0)
+Text_3.Size = UDim2.new(0, 675, 0, 38)
+Text_3.Font = Enum.Font.GothamSemibold
+Text_3.Text = " All the credits goes to Fate#4752, if the script doesn't work, make sure that the game your playing is supported by this hub, if not, request in discord."
+Text_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+Text_3.TextSize = 18.000
+Text_3.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Text_3.TextWrapped = true
+Text_3.TextXAlignment = Enum.TextXAlignment.Left
+Text_3.TextYAlignment = Enum.TextYAlignment.Top
+
+Button.Name = "Button"
+Button.Parent = main
+Button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Button.BackgroundTransparency = 1.000
+Button.Position = UDim2.new(0.410000026, 0, 0.826704681, 0)
+Button.Size = UDim2.new(0, 123, 0, 40)
+Button.Image = "rbxassetid://2790389767"
+Button.ImageColor3 = Color3.fromRGB(235, 235, 235)
+Button.ScaleType = Enum.ScaleType.Slice
+Button.SliceCenter = Rect.new(8, 8, 248, 248)
+Button.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/New-Exploiter/Release/main/QZ-Main.lua"))()
+	task.wait(0.05)
+	Fatex_Hub:Destroy()
+end)
+
+TextLabel.Parent = Button
+TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.491951227, 0, 0.49999994, 0)
+TextLabel.Size = UDim2.new(1, -5, 1, -5)
+TextLabel.Font = Enum.Font.GothamSemibold
+TextLabel.Text = "EXECUTE"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
+TextLabel_2.Parent = Button
+TextLabel_2.AnchorPoint = Vector2.new(0.5, 0.5)
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.47569108, 0, 0.49999994, 0)
+TextLabel_2.Size = UDim2.new(1, -5, 1, -5)
+TextLabel_2.Font = Enum.Font.GothamSemibold
+TextLabel_2.Text = "EXECUTE"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 14.000
+
+Frame_11.Name = "Frame"
+Frame_11.Parent = main
+Frame_11.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_11.BackgroundTransparency = 1.000
+Frame_11.Position = UDim2.new(0.0171428584, 0, 0.546363652, 0)
+Frame_11.Size = UDim2.new(0, 675, 0, 38)
+Frame_11.Image = "rbxassetid://2790382281"
+Frame_11.ImageColor3 = Color3.fromRGB(240, 240, 240)
+Frame_11.ScaleType = Enum.ScaleType.Slice
+Frame_11.SliceCenter = Rect.new(4, 4, 252, 252)
+
+Text_4.Name = "Text"
+Text_4.Parent = Frame_11
+Text_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Text_4.BackgroundTransparency = 1.000
+Text_4.Position = UDim2.new(0.00380952423, 0, -0.0138934851, 0)
+Text_4.Size = UDim2.new(0, 675, 0, 38)
+Text_4.Font = Enum.Font.GothamSemibold
+Text_4.Text = " Make sure that you have the updated version of the hub, the loadstring provided auto-updates and is the official github. If your not sure it works, use an alt."
+Text_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+Text_4.TextSize = 18.000
+Text_4.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Text_4.TextWrapped = true
+Text_4.TextXAlignment = Enum.TextXAlignment.Left
+Text_4.TextYAlignment = Enum.TextYAlignment.Top
+
+Text_5.Name = "Text"
+Text_5.Parent = main
+Text_5.BackgroundColor3 = Color3.fromRGB(240, 240, 240)
+Text_5.BorderColor3 = Color3.fromRGB(255, 255, 255)
+Text_5.Position = UDim2.new(0.3223809, 0, 0.719061136, 0)
+Text_5.Size = UDim2.new(0, 249, 0, 21)
+Text_5.Font = Enum.Font.GothamSemibold
+Text_5.Text = " GO ON AND DESTROY KIDS!"
+Text_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+Text_5.TextSize = 18.000
+Text_5.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Text_5.TextWrapped = true
+Text_5.TextXAlignment = Enum.TextXAlignment.Left
+Text_5.TextYAlignment = Enum.TextYAlignment.Top
+
+local UserInputService = game:GetService("UserInputService")
+
+local gui = main
+
+local dragging
+local dragInput
+local dragStart
+local startPos
+
+local function update(input)
+	local delta = input.Position - dragStart
+	gui.Position = gui:TweenPosition(UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y), 'Out', 'Quad', 0.25, true); -- drag speed
+end
+
+gui.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+		dragging = true
+		dragStart = input.Position
+		startPos = gui.Position
+
+		input.Changed:Connect(function()
+			if input.UserInputState == Enum.UserInputState.End then
+				dragging = false
+			end
+		end)
+	end
+end)
+
+gui.InputChanged:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+		dragInput = input
+	end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+	if input == dragInput and dragging then
+		update(input)
+	end
+end)
